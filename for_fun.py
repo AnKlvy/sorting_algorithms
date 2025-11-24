dbe8db2892b744e08ddfe5af7108c42e784af476
AnKlvy <andreyklimov.ver2@gmail.com>
2025-11-24 20:48:14 +0500

    triangles

---
+++ for_fun.py
@@ -0,0 +1,41 @@
+#        *
+#       * *
+#      *   *
+#     * * * *
+def triangle(n):
+    odd = 0
+    for i in range(n):
+        if i != n - 1:
+            if i > 1:
+                odd += 1
+            print((n - i - 1) * ' ' + '*' + (i + odd) * ' ' + ('*' if i != 0 else ''))
+        else:
+            print('* ' * n)
+
+# triangle(5)
+
+def triangle2(n):
+    odd = n*2-3
+    for i in range(n):
+        if i > 0:
+            if i < n:
+                odd -= 1
+            print(i * ' ' + '*' + (odd - i) * ' ' + ('*' if i != n-1 else ''))
+        else:
+            print('* ' * n)
+
+# triangle2(5)
+
+# *
+# **
+# * *
+# *  *
+# * * *
+# def triangle3(n):
+#     for i in range(n):
+#         if i != n-1:
+#             print('*' + (i) * ' ' + ('*' if i!=0 else ''))
+#         else:
+#             print('* ' * (n-2))
+#
+# triangle3(2)
