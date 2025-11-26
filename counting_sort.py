57151fed6d3f045f3fde2fa1881c7534406721ae
AnKlvy <andreyklimov.ver2@gmail.com>
2025-11-26 14:07:23 +0500

    actual implementation of counting sort

--- counting_sort.py
+++ counting_sort.py
@@ -1,36 +1,22 @@
-from collections import defaultdict
-
-
 def counting_sort(arr):
-    count_of_nums = defaultdict(int)
-    min_num=arr[0]
-    max_num=arr[0]
-    for i in range(len(arr)):
-        if min_num > arr[i]:
-            min_num = arr[i]
-        if max_num < arr[i]:
-            max_num = arr[i]
-        count_of_nums[arr[i]]+=1
-    print(dict(sorted(count_of_nums.items())))
+    if not arr:
+        return arr
 
-    for num in range(min_num, max_num):
-        count_of_nums[num+1] = count_of_nums[num] + count_of_nums[num+1]
-    print("after cumulative amount:")
-    print(dict(sorted(count_of_nums.items())))
+    max_val = max(arr)
+    min_val = min(arr)
+    count = [0] * (max_val - min_val + 1)
 
-    print(arr)
-    sorted_output = [0 for _ in range(len(arr))]
-    print(sorted_output)
     for num in arr:
-        print(num, ':', count_of_nums[num])
-        sorted_output[count_of_nums[num]-1] = num
-        count_of_nums[num] -= 1
+        count[num - min_val] += 1
+
+    arr[:] = []
+
+    for num, freq in enumerate(count):
+        arr.extend([num + min_val] * freq)
+
+    return arr
 
 
-    print("Finally:", sorted_output)
-    arr[:] = sorted_output
-
-arr = [3,4,-3,-8,5,6,-3,2,5]
+arr = [4, 2, 2, -6, -3, 3, -1, 6, 5, -2, 3]
 counting_sort(arr)
-
-print(arr)
+print("Sorted array:", arr)
